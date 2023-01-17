843559392b3ddcd462b53b2996f59372e52e70ab
Brian Sheridan <brian.sheridan@gmail.com>
2023-01-16 16:41:59 -0800

    Added Static Web App

--- infra/Program.cs
+++ infra/Program.cs
@@ -3,12 +3,25 @@ using Pulumi.AzureNative.Resources;
 using Pulumi.AzureNative.Storage;
 using Pulumi.AzureNative.Storage.Inputs;
 using System.Collections.Generic;
+using AzureNative = Pulumi.AzureNative;
+
 
 return await Pulumi.Deployment.RunAsync(() =>
 {
     // Create an Azure Resource Group
     var resourceGroup = new ResourceGroup("FlyballStats.com");
 
-    // Create an Azure resource (Storage Account)
-    
+    var staticWebApp = new AzureNative.Web.StaticSite("staticSite", new()
+    {
+        Location = resourceGroup.Location,
+        Name = "FlyballStats.com",
+        ResourceGroupName = resourceGroup.Name,
+        Sku = new AzureNative.Web.Inputs.SkuDescriptionArgs
+        {
+            Name = "Basic",
+            Tier = "Basic",
+        }, 
+        
+    });
+
 });
