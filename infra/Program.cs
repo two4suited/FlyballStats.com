129cfd2ca5c10706e2134cae5fd52a5654f866e7
Brian Sheridan <brian.sheridan@gmail.com>
2023-01-16 16:00:35 -0800

    Added Pulumi Project

---
+++ infra/Program.cs
@@ -0,0 +1,14 @@
+﻿using Pulumi;
+using Pulumi.AzureNative.Resources;
+using Pulumi.AzureNative.Storage;
+using Pulumi.AzureNative.Storage.Inputs;
+using System.Collections.Generic;
+
+return await Pulumi.Deployment.RunAsync(() =>
+{
+    // Create an Azure Resource Group
+    var resourceGroup = new ResourceGroup("FlyballStats.com");
+
+    // Create an Azure resource (Storage Account)
+    
+});
